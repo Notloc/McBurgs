69e087ff4d51251cc9e950e69377579f9a2865b9
Notloc <ColtonJamesCampbell@gmail.com>
2019-09-10 20:30:42 -0300

    Created ItemObject and ItemData classes

---
+++ Assets/Scripts/Items/ItemData.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Items/New Item")]
+public class ItemData : ScriptableObject
+{
+    [SerializeField] string itemName;
+    [SerializeField] bool edible;
+    [SerializeField] bool cookable;
+
+    public string Name { get { return itemName; } }
+    public bool IsEdible { get { return edible; } }
+    public bool IsCookable { get { return cookable; } }
+
+}
---
+++ Assets/Scripts/Items/ItemObject.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemObject : MonoBehaviour, IGrabbable
+{
+    [Header("Required References")]
+    [SerializeField] ItemData itemData;
+    [SerializeField] Rigidbody Rigidbody;
+
+    public void Interact()
+    {}
+
+    public virtual void Lock()
+    {
+        Rigidbody.isKinematic = true;
+    }
+
+    public virtual void Unlock()
+    {
+        Rigidbody.isKinematic = false;
+    }
+
+}
