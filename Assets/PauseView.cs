97854aaab44c212b0c2e4a9ce93753214aadffe4
Notloc <ColtonJamesCampbell@gmail.com>
2019-09-11 16:45:37 -0300

    Created a basic gui, has a pause menu that pauses and resumes the game

---
+++ Assets/PauseView.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseView : MonoBehaviour
+{
+    [Header("Required References")]
+    [SerializeField] Button resumeButton;
+
+    public bool IsPaused { get; private set; }
+
+    private void Awake()
+    {
+        resumeButton.onClick.AddListener(ResumeGame);
+    }
+
+    private void OnEnable()
+    {
+        _PauseGame();
+    }
+    private void OnDisable()
+    {
+        _ResumeGame();
+    }
+
+    private void _PauseGame()
+    {
+        Time.timeScale = 0f;
+        IsPaused = true;
+    }
+
+    private void _ResumeGame()
+    {
+        Time.timeScale = 1f;
+        IsPaused = false;
+    }
+
+    public void ResumeGame()
+    {
+        this.gameObject.SetActive(false);
+    }
+
+    public void PauseGame()
+    {
+        this.gameObject.SetActive(true);
+    }
+}
