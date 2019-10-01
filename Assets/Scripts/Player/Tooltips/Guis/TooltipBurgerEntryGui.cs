1e7e545793c94a69d6fe80272eb590e4f134fe52
Notloc <ColtonJamesCampbell@gmail.com>
2019-10-01 01:52:43 -0300

    Create TooltipManager, an abtract class that allows objects to have multiple tooltips and switch as needed.

---
+++ Assets/Scripts/Player/Tooltips/Guis/TooltipBurgerEntryGui.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+ class TooltipBurgerEntryGui : BaseTooltipEntryGui
+{
+    [Header("Required References")]
+    [SerializeField] ProgressBar vegetarianBar;
+    [SerializeField] ProgressBar cheesinessBar;
+
+    public override void AssignEntry(TooltipEntry entry)
+    {
+        this.entry = entry;
+        UpdateDisplay();
+    }
+
+    public override void UpdateDisplay()
+    {
+        // Try to get the value via reflection
+        try
+        {
+            Type reflectionType = entry.scriptReference.GetType();
+            var reflectedProperty = reflectionType.GetProperty(entry.valueName);
+
+            var value = reflectedProperty.GetValue(entry.scriptReference);
+            UpdateScores((BurgerScore)value);
+        }
+        catch (System.Exception e)
+        {
+            if (!entry.scriptReference)
+                Debug.LogError("No script reference provided for the entry.");
+            else
+                Debug.LogError(entry.valueName + " was not found in " + entry.scriptReference.name);
+        }
+    }
+
+    private void UpdateScores(BurgerScore score)
+    {
+        vegetarianBar.UpdateProgress(score.vegetarianScore);
+        cheesinessBar.UpdateProgress(score.chessiness);
+    }
+}
---
+++ Assets/Scripts/Player/Tooltips/TooltipManager.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class TooltipManager : MonoBehaviour
+{
+    public abstract IHaveTooltip ActiveTooltip { get; protected set; }
+}
