87bd02240037bf6d627e6fa2cecee514066e97c9
Notloc <ColtonJamesCampbell@gmail.com>
2019-10-07 19:25:20 -0300

    Added money to the GameController

---
+++ Assets/Scripts/GUI/MoneyGui.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MoneyGui : MonoBehaviour
+{
+    [SerializeField] Text moneyText;
+
+    private GameController gameController;
+    private void Awake()
+    {
+        gameController = GameController.Instance;
+    }
+
+    private void FixedUpdate()
+    {
+        SetMoneyText(gameController.Money);
+    }
+
+    private void SetMoneyText(float amount)
+    {
+        moneyText.text = "$" + amount.ToString("#.##");
+    }
+}
